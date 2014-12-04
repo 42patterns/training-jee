af6b36a9cd841b8e191d8099113a22de813dc006
kubamarchwicki <kuba@marchwicki.pl>
2014-12-05 00:43:47 +0100

    Extra 1: websocket realtime log information

--- exercises/feeds-reports/src/main/java/com/example/feeds/reports/FeedsReportWebService.java
+++ exercises/feeds-reports/src/main/java/com/example/feeds/reports/FeedsReportWebService.java
@@ -3,6 +3,8 @@ package com.example.feeds.reports;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
 import javax.jws.WebService;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,6 +16,9 @@ public class FeedsReportWebService {
 	@PersistenceContext
 	EntityManager em;
 	
+	@Inject
+	Event<ReportDTO> events;
+	
 	public ReportDTO[] reports() {
 		//select feed_title, sum(items_cnt) as cnt from reports group by create_date, feed_title order by cnt;
 		
@@ -22,6 +27,9 @@ public class FeedsReportWebService {
 				+ "group by dto.date, dto.title order by cnt desc")
 				.getResultList();
 		
+		for (ReportDTO dto: report) {
+			events.fire(dto);
+		}
 		return report.toArray(new ReportDTO[] {});
 	}
 }
